78514a1cc4f759ad4b61a8587ac9f54626f8e642
dhavalpurohit <dhavalpurohit94@gmail.com>
2018-11-21 15:34:10 +0530

    final update

--- wallpapers/settings.py
+++ wallpapers/settings.py
@@ -78,14 +78,21 @@ WSGI_APPLICATION = 'wallpapers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'portfoliomnthndb',
+#         'USER': 'mnthn',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliomnthndb',
-        'USER': 'mnthn',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -126,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/wallpapers/static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
